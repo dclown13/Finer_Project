8ceebfbb00e930c163fb4229c69105b1aafd33dc
dclown13 <w6092w@gmail.com>
2023-06-15 14:53:51 +0900

    06-15-1

--- Finer_Project/src/com/kh/member/controller/MemberContorller.java
+++ Finer_Project/src/com/kh/member/controller/MemberContorller.java
@@ -1,10 +1,10 @@
 package com.kh.member.controller;
 
-import com.kh.member.model.Member;
+import com.kh.finer_project.model.Members;
 
 public class MemberContorller {
 
-	private Member m = new Member();
+	private Members m = new Members();
 	
 	public void singUp() {
 		
