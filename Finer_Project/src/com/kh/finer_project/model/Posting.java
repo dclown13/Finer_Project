506c8aa9679546d57a8271a60a92bf0bcbc9af00
dclown13 <w6092w@gmail.com>
2023-06-15 16:02:14 +0900

    06-15-2

--- Finer_Project/src/com/kh/finer_project/model/Posting.java
+++ Finer_Project/src/com/kh/finer_project/model/Posting.java
@@ -1,5 +1,25 @@
 package com.kh.finer_project.model;
 
+import java.util.Scanner;
+
 public class Posting {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String post;
+		String change;
+		String list;
+		boolean delete;
+		String calldibs;
+		
+		
+	}
+}		
+
 	
-}
+	
+	
+	
+
