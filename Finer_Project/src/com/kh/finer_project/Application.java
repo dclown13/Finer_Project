f0c224bebf0f8477f02634b39f649e35fe830b46
dclown13 <w6092w@gmail.com>
2023-06-16 09:00:56 +0900

    06-16-2

--- Finer_Project/src/com/kh/finer_project/Application.java
+++ Finer_Project/src/com/kh/finer_project/Application.java
@@ -1,9 +1,49 @@
 package com.kh.finer_project;
 
+import java.util.Date;
+
 public class Application {
 
 	public static void main(String[] args) {
-
+		
+		
+		
+	}
+	
+	public void singUp() {
+		
+	}
+	public void login() {
+		
+	}
+	
+	public void itemName() {
+		
+		
 	}
 
+
+	public void date() {
+		
+		
+	}
+	
+	public void nick() {
+		
+		
+	}
+
+	
+	public void address() {
+		
+		
+	}
+
+	
+	public void price() {
+		
+		
+	}
+	
+	
 }
